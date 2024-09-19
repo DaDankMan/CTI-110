3ab903c2d776dc2b5643912d60f3747c89970986
DaDankMan <110858016+DaDankMan@users.noreply.github.com>
2024-09-19 10:27:48 -0400

    Add files via upload

---
+++ P1HW1_kugblenuSejiro.py
@@ -0,0 +1,21 @@
+#Sejiro K.
+#9/17/24
+#P1HW1
+#Math in Python
+
+print("-----Caclculating Exponents-----")
+print()
+print()
+
+base = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent value: "))
+
+print()
+print()
+
+int product = int(base) ** int(exponent)
+
+print(base + " raised to the power of " + exponent " is " + product + "!")
+
+print()
+print()
---
+++ P1LAB1_kugblenuSejiro.py
@@ -0,0 +1,9 @@
+#Sejiro Kugblenu
+#9/17/24
+#P1LAB1
+#Python input and output
+
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last name: ")
+
+print("Hello" , firstName , lastName , "! Welcome to CTI-110")
