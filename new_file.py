4b41f08fae81ca5a848e455161f9c208298dda60
DDM150 <110858016+DaDankMan@users.noreply.github.com>
2024-11-12 16:03:56 +0000

    behold, commit numero uno

---
+++ new_file.py
@@ -0,0 +1,10 @@
+
+def laCreatura(name, sound, hunger):
+    print(f"{name}s makes a {sound} noise.")
+    print(f"They have to eat {hunger}lbs of food per day.")
+
+def main():
+    laCreatura("snake", "hiss", 12)
+
+if __name__ == "__main__":
+    main()
